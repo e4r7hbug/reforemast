5ce62e1563d15f52fa39e6d6c32ccf78835e5630
Nate Tangsurat <e4r7hbug@gmail.com>
2019-01-14 15:17:46 -0800

    feat: Runnable Package example in main

---
+++ src/reforemast/__main__.py
@@ -0,0 +1,41 @@
+"""Reforemast demonstration entry point."""
+import logging
+import os
+
+from reforemast import Reforemast, updaters
+
+LOG = logging.getLogger(__name__)
+
+
+class TestApplicationUpdater(updaters.ApplicationUpdater):
+    """Example updater."""
+
+    @staticmethod
+    def match(application):
+        """Apply updates to any test Applications."""
+        return 'test' in application['name']
+
+
+def main():
+    """Entry point."""
+    gate_url = os.getenv('FOREMAST_GATE_URL')
+
+    reforemast = Reforemast()
+
+    reforemast.settings.gate_url = gate_url
+
+    reforemast.settings.application_updaters = [
+        TestApplicationUpdater,
+    ]
+
+    reforemast.run()
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+
+    LEVEL = int(os.getenv('REFOREMAST_LOG_LEVEL', str(logging.INFO)))
+    LOG.setLevel(LEVEL)
+    logging.getLogger('reforemast').setLevel(LEVEL)
+
+    main()
