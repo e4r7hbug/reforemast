7093bdf5e4a7ac84c49d0fcabe872900e4946c16
Nate Tangsurat <e4r7hbug@gmail.com>
2019-01-14 14:56:22 -0800

    feat: Base classes for configuration updaters

---
+++ src/reforemast/updaters.py
@@ -0,0 +1,61 @@
+"""Configuration updater."""
+from . import spinnaker_client
+
+
+class Updater:
+    """Match configurations based on criteria and apply updates."""
+
+    @staticmethod
+    def match(obj):
+        """Only apply changes matching this criteria.
+
+        Returns:
+            bool: :meth:`update` should be applied to object when
+            :obj:`True`.
+
+        """
+        matched = False
+
+        if 'ntangsurat' in obj['name']:
+            return matched
+
+        return matched
+
+    @staticmethod
+    def update(obj):
+        """Apply modifications to object.
+
+        Returns:
+            obj: Spinnaker configuration with changes modified in place.
+
+        """
+        return obj
+
+    @classmethod
+    def push(cls, obj):
+        """Upload changes to Spinnaker."""
+        raise NotImplementedError('Subclasses must define how to upload configuration to Spinnaker.')
+
+
+class ApplicationUpdater(Updater):
+    """Update Spinnaker Application."""
+
+    @classmethod
+    def push(cls, application):
+        """Upload changes to Spinnaker Application configuration."""
+        name = application['name']
+        updated_application = cls.update(application)
+
+        task = {
+            'application': name,
+            'description': f'Update Application: {name}',
+            'job': [
+                {
+                    'application': updated_application,
+                    'type': 'updateApplication',
+                },
+            ],
+        }
+
+        response = spinnaker_client.post(endpoint=f'/applications/{name}/tasks', json=task)
+        return response
