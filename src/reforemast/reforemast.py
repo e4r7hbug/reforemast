ac1584e3abc1dee4f75a26c2cbfe693f39a4a2c1
Nate Tangsurat <e4r7hbug@gmail.com>
2019-01-22 15:52:14 -0800

    refactor: Try to split out main stack of loops

--- src/reforemast/reforemast.py
+++ src/reforemast/reforemast.py
@@ -23,6 +23,8 @@ def confirm_and_apply(updater, auto_apply=False):
     """
     updated = False
 
+    LOG.debug('Applying Updater: %s', updater)
+
     diff = updater.diff_update()
     if diff:
         click.echo(diff)
@@ -41,29 +43,52 @@ class Reforemast:
     def __init__(self):
         self.settings = SETTINGS
 
-    def run(self):
-        """Iterate over Spinnaker Application and Pipeline configurations."""
+    def matched_application_updaters(self):
+        """Generate Application Updaters matching Spinnaker Applications."""
         for application in applications():
+            application_matched = False
+
             for application_updater in self.settings.application_updaters:
                 a_updater = application_updater(application)
 
                 if a_updater.match():
-                    click.secho(f'Application: {a_updater.name}', bold=True)
+                    if not application_matched:
+                        click.secho(f'Application: {a_updater.name}', bold=True)
+                        application_matched = True
 
                     a_updater.get()
 
-                    confirm_and_apply(a_updater, auto_apply=self.settings.auto_apply)
+                    yield a_updater
 
-                    for pipeline in pipelines(application):
-                        for pipeline_updater in self.settings.pipeline_updaters:
-                            p_updater = pipeline_updater(pipeline)
+    def matched_pipeline_updaters(self, application):
+        """Generate Pipeline Updaters matching Spinnaker Pipelines."""
+        for pipeline in pipelines(application):
+            for pipeline_updater in self.settings.pipeline_updaters:
+                p_updater = pipeline_updater(pipeline)
 
-                            if p_updater.match():
-                                confirm_and_apply(p_updater, auto_apply=self.settings.auto_apply)
+                if p_updater.match():
+                    yield p_updater
 
-                                for stage in pipeline['stages']:
-                                    for stage_updater in self.settings.stage_updaters:
-                                        s_updater = stage_updater(stage, parent_obj=pipeline)
+    def matched_stage_updaters(self, pipeline):
+        """Generate Stage Updaters matching Spinnaker Pipelines."""
+        for stage in pipeline['stages']:
+            for stage_updater in self.settings.stage_updaters:
+                s_updater = stage_updater(stage, parent_obj=pipeline)
 
-                                        if s_updater.match():
-                                            confirm_and_apply(s_updater, auto_apply=self.settings.auto_apply)
+                if s_updater.match():
+                    yield s_updater
+
+    def run(self):
+        """Iterate over Spinnaker Application and Pipeline configurations."""
+        LOG.debug('Apply Application Updaters: %s', self.settings.application_updaters)
+        LOG.debug('Apply Pipeline Updaters: %s', self.settings.pipeline_updaters)
+        LOG.debug('Apply Stage Updaters: %s', self.settings.stage_updaters)
+
+        for application_updater in self.matched_application_updaters():
+            confirm_and_apply(application_updater, auto_apply=self.settings.auto_apply)
+
+            for pipeline_updater in self.matched_pipeline_updaters(application_updater.application):
+                confirm_and_apply(pipeline_updater, auto_apply=self.settings.auto_apply)
+
+                for stage_updater in self.matched_stage_updaters(pipeline_updater.pipeline):
+                    confirm_and_apply(stage_updater, auto_apply=self.settings.auto_apply)
